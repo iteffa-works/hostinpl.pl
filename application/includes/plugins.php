be4337a173ac0d682bbb5bbdddb8b9f54027fad8
Микола Довгопол (iTeffa) <97382411+iteffa-works@users.noreply.github.com>
2026-07-06 02:43:41 +0300

    feat: add plugins registry and ignore runtime settings.json

---
+++ application/includes/plugins.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Dovhopol Mykola Ivanovich (iTeffa)
+ * Telegram: https://t.me/iteffa
+ * Phone: +380966349498
+ * Email: flowaxy.dev@gmail.com
+ * Website: https://flowaxy.com/
+ *
+ * Реестр ID плагинов панели.
+ *
+ * Created: 2026-07-02
+ * Modified: 2026-07-06
+ *
+ * © 2026 Flowaxy Digital Studio. All rights reserved.
+ */
+
+return array(
+	'site-landing',
+);
